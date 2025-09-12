956538c27b41c105a13c144e4f4b010ebae0f0ce
Matin Ghanbari <ghanbarimatin6@gmail.com>
2025-09-12 16:32:40 +0330

    Resolve #3

--- net8.0/src/NexGen.MediatR.Extensions.Caching.Redis/Extensions/RedisConfigurationExtensions.cs
+++ net8.0/src/NexGen.MediatR.Extensions.Caching.Redis/Extensions/RedisConfigurationExtensions.cs
@@ -5,7 +5,7 @@ using NexGen.MediatR.Extensions.Caching.Enums;
 
 namespace NexGen.MediatR.Extensions.Caching.Redis.Extensions;
 
-public static class RequestOutputCacheConfigurationOptionExtensions
+public static class RedisConfigurationExtensions
 {
     public static void UseRedisCache(this RequestOutputCacheConfigurationOption options, string connectionString)
     {
