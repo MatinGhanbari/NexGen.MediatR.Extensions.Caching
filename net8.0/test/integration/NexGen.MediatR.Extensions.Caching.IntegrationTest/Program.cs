e4859688e42f7bff1442eb0270c3ba0b30e35c27
Matin Ghanbari <ghanbarimatin6@gmail.com>
2025-09-12 17:11:03 +0330

    Update tests

--- net8.0/test/integration/NexGen.MediatR.Extensions.Caching.IntegrationTest/Program.cs
+++ net8.0/test/integration/NexGen.MediatR.Extensions.Caching.IntegrationTest/Program.cs
@@ -5,6 +5,7 @@ using NexGen.MediatR.Extensions.Caching.Configurations;
 using NexGen.MediatR.Extensions.Caching.Garnet.Extensions;
 using NexGen.MediatR.Extensions.Caching.IntegrationTest.WeatherForecasts;
 using NexGen.MediatR.Extensions.Caching.IntegrationTest.WeatherForecasts.GetWeatherForecasts;
+using NexGen.MediatR.Extensions.Caching.Redis.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,9 +18,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddMediatR(opt => opt.RegisterServicesFromAssembly(typeof(WeatherForecastRequest).Assembly));
 builder.Services.AddMediatROutputCache(opt =>
 {
-    //opt.UseMemoryCache();
-    // opt.UseRedisCache(builder.Configuration.GetConnectionString("Redis")!);
-     opt.UseGarnetCache(builder.Configuration.GetConnectionString("Garnet")!);
+    opt.UseMemoryCache();
+    //opt.UseRedisCache(builder.Configuration.GetConnectionString("Redis")!);
+    //opt.UseGarnetCache(builder.Configuration.GetConnectionString("Garnet")!);
 });
 
 var app = builder.Build();
