267bfdc70c4c94c96e3126927daac05f6c55ad44
Matin Ghanbari <ghanbarimatin6@gmail.com>
2025-09-12 18:08:56 +0330

    Add docs for GarnetConfigurationExtensions

--- net8.0/src/NexGen.MediatR.Extensions.Caching/Constants/ErrorMessages.cs
+++ net8.0/src/NexGen.MediatR.Extensions.Caching/Constants/ErrorMessages.cs
@@ -7,4 +7,6 @@ public class ErrorMessages
 {
     public static readonly string ResponseNotFound = "Response not found";
     public static readonly string CacheHit = "Cache hit for MediatR request {RequestName}, Returning cached response.";
+    public static readonly string AlreadyConfigured = "The request output cache has already been configured.";
+    public static readonly string EmptyConnectionString = "The connection string cannot be empty.";
 }
